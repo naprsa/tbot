f982b18add7bd5b6e66111196e359bae9000451e
Cupera <napr.sa@gmail.com>
2021-01-18 17:40:58 +0300

    middlewares throttling added

--- src/app.py
+++ src/app.py
@@ -4,10 +4,10 @@ from utils.set_bot_commands import set_default_commands
 
 async def on_startup(dp: Dispatcher) -> None:
     import filters
+    import middlewares
 
-    #     import middlewares
     filters.setup(dp)
-    #     middlewares.setup(dp)
+    middlewares.setup(dp)
     from utils.notify_admins import on_startup_notify
 
     await on_startup_notify(dp)
