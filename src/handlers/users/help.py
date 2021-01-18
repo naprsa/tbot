ef1c8bf90e6f5cccfe64d4e2475b0c52c3ef7c5a
Cupera <napr.sa@gmail.com>
2021-01-18 17:42:20 +0300

    user class added, database middleware added

--- src/handlers/users/help.py
+++ src/handlers/users/help.py
@@ -3,10 +3,10 @@ from aiogram.dispatcher.filters.builtin import CommandHelp
 
 from src.loader import dp
 
-# from utils.misc import rate_limit
+from src.utils.misc import rate_limit
 
 
-# @rate_limit(5, "help")
+@rate_limit(limit=10)
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ["Список команд: ", "/start - Начать диалог", "/help - Получить справку"]
